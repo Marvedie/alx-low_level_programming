ef857a6b64c05caed818eb50f7150773dbf4d3fc
Marvedie <marvin.mwenda13@gmail.com>
2023-08-10 13:14:46 -0700

    Correction

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,41 +1,22 @@
 #include "main.h"
 
 /**
- * _memset - Copies a specific character to each element of an array.
- * @dest: Pointer to the memory area to fill.
- * @ch: Character value to set.
- * @n: Number of bytes to fill.
- * Return: A pointer to the filled memory area.
+ * array_range - creates an array of integers
+ * @min: minimum
+ * @max: maximum
+ * Return: array
  */
-char *_memset(char *dest, char ch, unsigned int n)
+int *array_range(int min, int max)
 {
-	unsigned int index;
+	int *arr, i = 0, t = min;
 
-	for (index = 0; index < n; index++)
-	{
-		dest[index] = ch;
-	}
-	return (dest);
-}
+	if (min > max)
+		return (0);
+	arr = malloc((max - min + 1) * sizeof(int));
 
-/**
- * _calloc - Allocates memory for an array using malloc.
- * @num_elements: Number of elements in the array.
- * @element_size: Size of each element.
- * Return: A pointer to the allocated memory, or NULL if allocation fails.
- */
-void *_calloc(unsigned int num_elements, unsigned int element_size)
-{
-	void *allocated_memory;
-
-	if (num_elements == 0 || element_size == 0)
-		return (NULL);
-	
-	allocated_memory = malloc(num_elements * element_size);
-
-	if (allocated_memory == NULL)
-		return (NULL);
-
-	_memset(allocated_memory, 0, (num_elements * element_size));
-	return (allocated_memory);
+	if (!arr)
+		return (0);
+	while (i <= max - min)
+		arr[i++] = t++;
+	return (arr);
 }
